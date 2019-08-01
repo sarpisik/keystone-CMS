b195871b0fce369875e865e9c3e8288095e21041
sarpisik <contact@sarpisik.com>
2019-08-01 14:53:17 +0300

    Add NextJS

--- keystone.js
+++ keystone.js
@@ -2,18 +2,19 @@
 // customising the .env file in your project's root folder.
 require('dotenv').config();
 
-// Require keystone
-var keystone = require('keystone');
+// Next app
+const next = require('next'),
+	dev = process.env.NODE_ENV !== 'production',
+	app = next({ dev }),
+	// Require keystone
+	keystone = require('keystone');
 
+// Initialise Keystone with your project's configuration.
+// See http://keystonejs.com/guide/config for available options
+// and documentation.
 keystone.init({
 	'name': 'my-cms',
 	'brand': 'my-cms',
-
-	'less': 'public',
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'templates/views',
-	'view engine': 'pug',
 
 	'emails': 'templates/emails',
 
@@ -23,33 +24,41 @@ keystone.init({
 	'user model': 'User',
 	'cookie secret': '7843oaerngdsm,.bgfl;kewaoipqw89y7q24ht54wkhry;klduy8gty',
 });
+
+// Models
 keystone.import('models');
-keystone.set('session store', 'mongo');
-keystone.set('locals', {
-	_: require('lodash'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable,
+
+// Start Next App
+app.prepare().then(() => {
+	keystone.set('session store', 'mongo');
+	// keystone.set('locals', {
+	// 	_: require('lodash'),
+	// 	env: keystone.get('env'),
+	// 	utils: keystone.utils,
+	// 	editable: keystone.content.editable,
+	// });
+
+	// Load your project's Routes
+	keystone.set('routes', require('./routes')(app));
+
+	keystone.set('nav', {
+		posts: ['posts', 'post-categories'],
+		galleries: 'galleries',
+		enquiries: 'enquiries',
+		users: 'users',
+	});
+
+	if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+		console.log(
+			'----------------------------------------'
+				+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
+				+ '\n----------------------------------------'
+				+ '\nYou have opted into email sending but have not provided'
+				+ '\nmailgun credentials. Attempts to send will fail.'
+				+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
+				+ '\nset up your mailgun integration'
+		);
+	}
+
+	keystone.start();
 });
-keystone.set('routes', require('./routes'));
-
-keystone.set('nav', {
-	posts: ['posts', 'post-categories'],
-	galleries: 'galleries',
-	enquiries: 'enquiries',
-	users: 'users',
-});
-
-if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
-	console.log(
-		'----------------------------------------'
-			+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
-			+ '\n----------------------------------------'
-			+ '\nYou have opted into email sending but have not provided'
-			+ '\nmailgun credentials. Attempts to send will fail.'
-			+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
-			+ '\nset up your mailgun integration'
-	);
-}
-
-keystone.start();
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,23 @@
-var keystone = require('keystone');
-var middleware = require('./middleware');
-var importRoutes = keystone.importer(__dirname);
-
-// Common Middleware
-keystone.pre('routes', middleware.initErrorHandlers);
-keystone.pre('routes', middleware.initLocals);
-keystone.pre('render', middleware.flashMessages);
-
-// Handle 404 errors
-keystone.set('404', function (req, res, next) {
-	res.notfound();
-});
-
-// Handle other errors
-keystone.set('500', function (err, req, res, next) {
-	var title, message;
-	if (err instanceof Error) {
-		message = err.message;
-		err = err.stack;
-	}
-	res.err(err, title, message);
-});
-
-// Import Route Controllers
-var routes = {
-	views: importRoutes('./views'),
-};
+const keystone = require('keystone');
 
 // Setup Route Bindings
-exports = module.exports = function (app) {
-	// Views
-	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
-	app.get('/gallery', routes.views.gallery);
-	app.all('/contact', routes.views.contact);
+exports = module.exports = nextApp => keystoneApp => {
+	// Next request handler
+	const handle = nextApp.getRequestHandler();
+
+	keystoneApp.get('/api/posts', (req, res, next) => {
+		const Post = keystone.list('Post');
+		Post.model
+			.find()
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.exec(function (err, results) {
+				if (err) throw err;
+				res.json(results);
+			});
+	});
+
+	keystoneApp.get('*', (req, res) => {
+		return handle(req, res);
+	});
 };
